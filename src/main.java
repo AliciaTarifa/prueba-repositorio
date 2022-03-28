4c857044e9b0ab97e2951669e5aea3c2cb0f21ab
Alicia <alicia.tarifa.8@gmail.com>
2022-03-28 15:51:22 -0300

    Mi primera subida

---
+++ src/main.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+
+/**
+ * @author luise *
+ */
+
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int num1 = 5;
+		int num2 = 3;
+		int result = num1 + num2;
+		mi_primer_objeto operaciones = new mi_primer_objeto();
+		
+		// TODO Auto-generated method stub
+		System.out.println("Hola Alicia");
+		System.out.println("La suma de " + num1 + " y " + num2 + " es: " + result);
+		System.out.println("La suma de " + operaciones.a + " y " + operaciones.b + " es: " + operaciones.suma());
+		System.out.println("La multiplicacion de " + operaciones.a + " y " + operaciones.b + " es: " + operaciones.multiplica());
+		System.out.println("La division de " + operaciones.a + " y " + operaciones.b + " es: " + operaciones.divide());
+		System.out.println(operaciones.a + " es mayor que " + operaciones.b + " ? >>> " + operaciones.mayor());
+
+	}
+
+}
---
+++ src/mi_primer_objeto.java
@@ -0,0 +1,21 @@
+
+public class mi_primer_objeto {
+	protected int a = 10;
+	protected int b = 2;
+	
+	public int suma() {
+		return a+b;
+	}
+	
+	public int multiplica() {
+		return a*b;
+	}
+	
+	public double divide() {
+		return a/b;
+	}
+	
+	public boolean mayor() {
+		return a > b;
+	}
+}
